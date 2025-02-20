e3d64661ceead33dc1ffc6ef633c11aea37d54f3
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-20 16:41:45 +0530

    Improvement 04

--- src/components/SOS/BottomSection.jsx
+++ src/components/SOS/BottomSection.jsx
@@ -6,8 +6,8 @@ function BottomSection() {
                 <i class="fa-solid fa-phone-volume text-3xl"></i>
             </button>
 
-            <botton className="relative bg-white w-max text-[#183154] rounded-full flex items-center justify-center p-4 group">
-                    <span className="absolute invisible group-hover:visible w-max bg-[#183154] text-white px-2 py-1 rounded-md text-xs top-2 group-hover:-top-10 left-1/2 -translate-x-1/2 after:content-[''] after:w-3 after:h-3 after:bg-inherit after:rotate-45 after:absolute after:left-1/2 after:-translate-x-1/2 after:-bottom-1 after:-z-10 transition-all duration-150 ease-in-out">Chat Bot</span>
+            <botton className="relative bg-[#fff] w-max text-[#1570bf] rounded-full flex items-center justify-center p-3 group">
+                    <span className="absolute invisible group-hover:visible w-max bg-[#1570bf] text-white px-2 py-1 rounded-md text-xs top-2 group-hover:-top-10 left-1/2 -translate-x-1/2 after:content-[''] after:w-3 after:h-3 after:bg-inherit after:rotate-45 after:absolute after:left-1/2 after:-translate-x-1/2 after:-bottom-1 after:-z-10 transition-all duration-150 ease-in-out">Chat Bot</span>
                     {/* <i class="fa-solid fa-comment-medical text-3xl"></i> */}
                     <i class="fa-solid fa-message text-2xl"></i>
             </botton>
