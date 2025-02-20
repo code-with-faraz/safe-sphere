5b82af5609886375d79667c6841f4e31229af83c
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-20 16:56:33 +0530

    Improvement 05

--- src/components/SOS/BottomSection.jsx
+++ src/components/SOS/BottomSection.jsx
@@ -1,19 +1,19 @@
 function BottomSection() {
 
     return (
-        <div className="flex items-center justify-center w-full px-4 py-2 gap-28 mb-4">
+        <div className="flex items-center justify-center w-max px-4 py-2 gap-8 sm:gap-28 mb-4">
             <button className="flex items-center">
-                <i class="fa-solid fa-phone-volume text-3xl"></i>
+                <i class="fa-solid fa-phone-volume text-2xl sm:text-3xl"></i>
             </button>
 
             <botton className="relative bg-[#fff] w-max text-[#1570bf] rounded-full flex items-center justify-center p-3 group">
                     <span className="absolute invisible group-hover:visible w-max bg-[#1570bf] text-white px-2 py-1 rounded-md text-xs top-2 group-hover:-top-10 left-1/2 -translate-x-1/2 after:content-[''] after:w-3 after:h-3 after:bg-inherit after:rotate-45 after:absolute after:left-1/2 after:-translate-x-1/2 after:-bottom-1 after:-z-10 transition-all duration-150 ease-in-out">Chat Bot</span>
                     {/* <i class="fa-solid fa-comment-medical text-3xl"></i> */}
-                    <i class="fa-solid fa-message text-2xl"></i>
+                    <i class="fa-solid fa-message text-xl sm:text-2xl"></i>
             </botton>
 
             <botton className="flex items-center">
-                <i class="fa-solid fa-gear text-3xl"></i>
+                <i class="fa-solid fa-gear text-2xl sm:text-3xl"></i>
             </botton>
 
         </div>
--- src/components/SOS/SOSbutton.jsx
+++ src/components/SOS/SOSbutton.jsx
@@ -10,8 +10,8 @@ const SOSButton = () => {
         <div className="absolute w-36 h-36 rounded-full border-4 border-red-600 animate-ping"></div> */}
         
         {/* Main Button */}
-          <button className="w-72 h-72 text-white text-2xl font-bold flex justify-center items-center shadow-lg active:scale-95 transition transform">
-            <p className="absolute z-10 text-5xl">SOS</p>
+          <button className="w-52 sm:w-72 h-52 sm:h-72 text-white text-2xl font-bold flex justify-center items-center shadow-lg active:scale-95 transition transform">
+            <p className="absolute z-10 text-3xl sm:text-5xl">SOS</p>
             <img src={logo} alt="logo" className="w-max absolute" />
         </button>
       </div>
