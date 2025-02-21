935e7b08011ca75e77cae304e750cadd79968128
Faraz Mirza <farazmirza1023@gmail.com>
2025-02-22 00:03:28 +0530

    Added a log in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import HomeScreen from "./components/Home/HomeScreen";
+import Login from "./components/login/Login";
 
 const App = () => {
+  const user = true;
 
   return (
-    <div className='container'>
-
-      <>
-        <HomeScreen/>
-      </>
-
+    <div className="container">
+      {user ? (
+        <>
+          <HomeScreen />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
