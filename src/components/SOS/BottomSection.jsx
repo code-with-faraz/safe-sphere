35777fb5c68978a3b76159e7a2a055cdf80cb04c
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-22 09:16:29 +0530

    Improvement 07

--- src/components/SOS/BottomSection.jsx
+++ src/components/SOS/BottomSection.jsx
@@ -3,7 +3,7 @@ function BottomSection() {
     return (
         <div className="flex items-center justify-center w-max px-4 py-2 gap-8 sm:gap-28 mb-4">
             <button className="flex items-center">
-                <i class="fa-solid fa-phone-volume text-2xl sm:text-3xl"></i>
+                <i class="fa-solid fa-phone-volume text-2xl sm:text-3xl text-[#fff] hover:text-[#d9d9d9]"></i>
             </button>
 
             <botton className="relative bg-[#fff] w-max text-[#1570bf] rounded-full flex items-center justify-center p-3 group">
@@ -13,7 +13,7 @@ function BottomSection() {
             </botton>
 
             <botton className="flex items-center">
-                <i class="fa-solid fa-gear text-2xl sm:text-3xl"></i>
+                <i class="fa-solid fa-gear text-2xl sm:text-3xl text-[#fff] hover:text-[#d9d9d9]"></i>
             </botton>
 
         </div>
--- src/components/SOS/TopSection.jsx
+++ src/components/SOS/TopSection.jsx
@@ -5,7 +5,7 @@ function TopSection() {
     return (
         <div className="flex items-center justify-center w-full px-4 py-2 gap-8 sm:gap-12 mt-4">
             <botton className="flex items-center">
-                <i className="fa-solid fa-bell text-2xl sm:text-3xl"></i>
+                <i className="fa-solid fa-bell text-2xl sm:text-3xl text-[#fff] hover:text-[#d9d9d9]"></i>
             </botton>
 
             <div className="flex items-center gap-2 sm:gap-4 bg-[#d9d9d9] text-[#111] px-3 sm:px-4 py-2 rounded-md">
@@ -14,7 +14,7 @@ function TopSection() {
             </div>
 
             <botton className="flex items-center">
-                <i className="fa-solid fa-user text-2xl sm:text-3xl"></i>
+                <i className="fa-solid fa-user text-2xl sm:text-3xl text-[#fff] hover:text-[#d9d9d9]"></i>
             </botton>
         </div>
     );
