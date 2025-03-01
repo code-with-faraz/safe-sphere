3282faba2693e5e9f160cb8af6a0794fdcc10d23
Faraz Mirza <farazmirza1023@gmail.com>
2025-03-01 10:13:50 +0530

    made changes in bottom section

--- src/components/SOS/BottomSection.jsx
+++ src/components/SOS/BottomSection.jsx
@@ -6,16 +6,9 @@ function BottomSection() {
 
     return (
         <div className="flex items-center justify-center w-max px-4 py-2 gap-14 sm:gap-28 mb-4">
-
-            <botton className="relative bg-[#fff] w-max text-[#1570bf] rounded-full flex items-center justify-center p-3 group active:scale-95">
-                    <span className="absolute invisible group-hover:visible w-max bg-[#1570bf] text-[#d9d9d9] px-2 py-1 rounded-md text-xs top-2 group-hover:-top-10 left-1/2 -translate-x-1/2 after:content-[''] after:w-3 after:h-3 after:bg-inherit after:rotate-45 after:absolute after:left-1/2 after:-translate-x-1/2 after:-bottom-1 after:-z-10 transition-all duration-150 ease-in-out">Chat Bot</span>
-                    <i class="fa-solid fa-message text-xl sm:text-2xl"></i>
-            </botton>
-
-            <botton className="flex items-center active:scale-95">
-                <i class="fa-solid fa-gear text-2xl sm:text-3xl text-[#fff] hover:text-[#d9d9d9]"></i>
-            </botton>
-
+            <FakeCall />
+            <ChatBot />
+            <Setting />
         </div>
     );
 }
