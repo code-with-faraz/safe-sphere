e7f03bc44ba44a711e0aa84ceb960b299d44543f
Faraz Mirza <farazmirza1023@gmail.com>
2025-04-04 20:36:52 +0530

    Login & Signup

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
 
   const login = () => {
-    fetch("http://localhost:5050/auth/login", {
+    fetch("https://backend-sage-seven-61.vercel.app/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
--- src/components/login/signup.jsx
+++ src/components/login/signup.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch("http://localhost:5050/auth/signup", {
+      const response = await fetch("https://backend-sage-seven-61.vercel.app/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
