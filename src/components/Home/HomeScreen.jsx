ece8d1be0fecb44e745c14c57915306358af7e75
Faraz Mirza <farazmirza1023@gmail.com>
2025-04-06 14:42:56 +0530

    Update HomeScreen.jsx

--- src/components/Home/HomeScreen.jsx
+++ src/components/Home/HomeScreen.jsx
@@ -1,17 +1,18 @@
 import SOSButton from "../SOS/SOSbutton";
 import TopSection from "../SOS/TopSection";
 import BottomSection from "../SOS/BottomSection";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+
 const HomeScreen = () => {
-  const navigate=useNavigate();
- 
-  useEffect(() => {
-    if(!localStorage.getItem("token"))
-    {
-      navigate("/login");
-    }
-  }, []);
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (!localStorage.getItem("token")) {
+  //     navigate("/login");
+  //   }
+  // }, []);
+
   return (
     <div className="flex flex-col items-center justify-between w-full overflow-hidden">
       <TopSection />
